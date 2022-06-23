2a4e8c81d22d554b0bbbabe90643c83339a14078
Vyacheslav1911 <slavagavrilov99@gmail.com>
2022-06-23 08:36:20 +0300

    hw-mile-Bonus

---
+++ src/Mile_1.java
@@ -0,0 +1,13 @@
+public class Mile_1 {
+    public static void main(String[] args) {
+
+        int pay = 50;
+        int bonus = 20;
+        int result = pay/bonus;
+
+
+
+        System.out.println(result);
+
+    }
+}
