c0a7bd62f6684da209911f540c2323f0f7882ab0
Vyacheslav1911 <slavagavrilov99@gmail.com>
2022-06-26 11:49:43 +0300

    rename and adit

--- src/Mile1.java
+++ src/Mile1.java
@@ -1,11 +1,9 @@
-public class Mile_1 {
+public class Mile1 {
     public static void main(String[] args) {
 
         int pay = 50;
         int bonus = 20;
         int result = pay/bonus;
-
-
 
         System.out.println(result);
 
